cdc788a3fdcc8d659eacd95c7c953bb016042518
2500959 <2500959@1utar.my>
2026-04-08 18:06:19 +0800

    Test

--- src/booking/Booking.java
+++ src/booking/Booking.java
@@ -2,4 +2,13 @@ package booking;
 
 public class Booking {
 
+	private String bookingId;
+	private String userId;
+	private String facilityId;
+	private TimeSlot timeSlot;
+	private String purpose;
+	private String status;
+	private String createdTime;
+	private String lastModifiedTime;
+
 }
