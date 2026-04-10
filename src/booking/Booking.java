9ce50599f9b496e4c0419c4c042e7b78a75124f9
2500959 <2500959@1utar.my>
2026-04-10 18:15:45 +0800

    1

--- src/booking/Booking.java
+++ src/booking/Booking.java
@@ -1,5 +1,7 @@
 package booking;
 
+// get current/now date + time
+import java.time.LocalDateTime;
 public class Booking {
 
 	private String bookingId;
@@ -10,5 +12,36 @@ public class Booking {
 	private String status;
 	private String createdTime;
 	private String lastModifiedTime;
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
+	public boolean canModify() {
+		if(status == "Pending") {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// get current/now date + time
+	LocalDateTime now = LocalDateTime.now();
+	
+	public boolean canCancel() {
+		// now date + time > booking start time
+		if(now.isAfter(timeSlot)) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	public void modifyBooking(TimeSlot newTimeSlot, String newPurpose) {
+		timeSlot = newTimeSlot;
+		purpose = newPurpose;
+	}
+	
 }
--- src/booking/BookingManager.java
+++ src/booking/BookingManager.java
@@ -2,4 +2,14 @@ package booking;
 
 public class BookingManager {
 
+	private List<Booking> bookings;
+	
+	private BookingStorage bookingStorage;
+	
+	public void createBooking(Booking booking) {
+		if(canModify()) {
+			
+		}
+	}
+	
 }
