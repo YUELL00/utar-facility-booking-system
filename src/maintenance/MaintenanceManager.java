d32293b6da8c1ca979a6d321d5086c794e63de47
user <user@172.16.141.122>
2026-04-14 11:31:56 +0800

    feat(maintenanceReport): add constructor and getter

--- src/maintenance/MaintenanceManager.java
+++ src/maintenance/MaintenanceManager.java
@@ -1,5 +1,36 @@
 package maintenance;
 
+import java.util.*;
 public class MaintenanceManager {
+	private List<MaintenanceReport> reports;
+	private MaintenanceStorage maintenanceStorage;
+	
+	public void createMaintenanceReport(MaintenanceReport report) {
+		//WIP
+	}
+	
+	public void assignMaintenance(String reportId, String userId) {
+		//WIP
+		//status = "Assigned";
+	}
+	public void updateMaintenanceStatus(String reportId, String status) {
+		//WIP
+	}
+	public MaintenanceReport getMaintenanceHistory(){
+		//WIP
+	}
+	public void getFrequentIssues() {
+		//WIP
+	}
+	public void generateMaintenanceReport() {
+		//WIP
+	}
+	public void loadReports() {
+		//WIP
+	}
+	public void saveReports() {
+		//WIP
+	}
+
 
 }
