d17bf2b03ab9fac1f29b5ddc4a6c8563da25b15f
User <User@172.16.135.199>
2026-04-22 18:33:40 +0800

    fix bug

--- src/util/NotificationService.java
+++ src/util/NotificationService.java
@@ -1,9 +1,21 @@
 package util;
 
-public class NotificationService {
+import booking.Booking;
 
-	public static String generateBookingReminder(String bookingId, String facilityId, 
-														String date, String startTime) {
+public class NotificationService {
+	
+	public NotificationService() {}
+
+	public String generateBookingReminder(Booking b) {
+		
+		if (b == null) {
+			return "Error: Booking data is missing.";
+		}
+		
+		String bookingId = b.getBookingId();
+		String facilityId = b.getFacilityId();
+		String date = b.getDate();
+		String startTime = b.getStartTime();
 		
 		String message = "Reminder: Booking " + bookingId + " for facility " + 
 						facilityId + " is scheduled on " + date + " at " + startTime + ".";
