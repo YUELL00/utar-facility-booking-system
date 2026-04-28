ecac8d8e871a9d8f329c78d0930069a82cf7cd7d
User <User@172.16.135.199>
2026-04-28 10:20:23 +0800

    fix bug, related class: Student, UserStorage

--- src/user/Student.java
+++ src/user/Student.java
@@ -2,9 +2,16 @@ package user;
 
 public class Student extends User{
 
+	private String programme;
+	
 	public Student(String userId, String password, String name,
 			String facultyOrDepartment, String contactNumber, String programme){
 		super(userId, password, name, facultyOrDepartment, contactNumber);
+		this.programme = programme;
+	}
+	
+	public String getProgramme() {
+		return programme;
 	}
 	
 	@Override
