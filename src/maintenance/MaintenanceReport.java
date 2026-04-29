b74e526dac5f95aac553bb0a916fdd8dd39aa837
User <User@LAPTOP-HROE1EHJ>
2026-04-29 20:48:14 +0800

    fix(maintenancReport): logical error

--- src/maintenance/MaintenanceReport.java
+++ src/maintenance/MaintenanceReport.java
@@ -1,46 +1,59 @@
 package maintenance;
 
 public class MaintenanceReport {
+	
 	private String reportId;
 	public String getReportId() {
 		return reportId;
 	}
+	
 	private String facilityId;
 	public String getFacilityId() {
 		return facilityId;
 	}
+	
 	private String reportedByUserId;
 	public String getReportedByUserId() {
 		return reportedByUserId;
 	}
+	
 	private String assignedByUserId;
-	private String getAssignedByUserId() {
+	public String getAssignedByUserId() {
 		return assignedByUserId;
 	}
+	
 	private String description;
 	public String getDescription() {
 		return description;
 	}
+	
 	private String reportDate;
 	public String getReportDate() {
 		return reportDate;
 	}
+	
 	private String startDate;
 	public String getStartDate() {
 		return startDate;
 	}
+	
 	private String endDate;
 	public String getEndDate() {
 		return endDate;
 	}
+	
 	private String status;
 	public String getStatus() {
 		return status;
 	}
 	
+	private String priority;
+	public String getPriority() {
+		return priority;
+	}
+	
 	//constructor
-	//Maintenance Report
-	public MaintenanceReport(String reportId, String facilityId, String reportedByUserId, String assignedByUserId, String description, String reportDate, String startDate, String endDate, String status) {
+	public MaintenanceReport(String reportId, String facilityId, String reportedByUserId, String assignedByUserId, String description, String reportDate, String startDate, String endDate, String status, String priority) {
 		this.reportId = reportId;
 		this.facilityId = facilityId;
 		this.reportedByUserId = reportedByUserId;
@@ -50,34 +63,24 @@ public class MaintenanceReport {
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.status = status;
-	}
-	
-	//Feedback
-	public MaintenanceReport(String reportId, String facilityId, String reportedByUserId, String description, String reportDate) {
-		this.reportId = reportId;
-		this.facilityId = facilityId;
-		this.reportedByUserId = reportedByUserId;
-		this.description = description;
-		this.reportDate = reportDate;
+		this.priority = priority;
 	}
 	
 	public void updateTaskStatus(String status) {
 		this.status = status;
 	}
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
-	}
+	
 	public void assignTo(String userId) {
-		this.reportedByUserId = userId;
+		//WIP
+		this.assignedByUserId = userId;
 	}
+	
 	public boolean isCompleted() { //WIP 
-		if(status == "COMPLETED") {
+		if("COMPLETED".equals(status)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
+	
 }
