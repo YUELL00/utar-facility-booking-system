7a2e27c5512b2aa3ccf6546f59cfb7e9063c7a04
User <User@LAPTOP-HROE1EHJ>
2026-04-29 21:52:24 +0800

    fix(FacilityManager, MaintenanceManager)

--- src/maintenance/MaintenanceManager.java
+++ src/maintenance/MaintenanceManager.java
@@ -2,21 +2,38 @@ package maintenance;
 
 import java.time.*;
 import java.util.*;
+
+import storage.MaintenanceStorage;
+
 public class MaintenanceManager {
 	private ArrayList<MaintenanceReport> reports;
 	private int size;
 	private MaintenanceStorage maintenanceStorage;
 	
+	public MaintenanceManager() {
+		reports = new ArrayList<>();
+		maintenanceStorage = new MaintenanceStorage("maintenance.txt");
+		size = 0;
+	}
+	
 	public void createMaintenanceReport() { //WIP
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter the facility Id (Ex: F001): ");
-		String facilityId = new Scanner(System.in).nextLine();
+		String facilityId = sc.nextLine();
+		
 		System.out.println("Enter the issue description: ");
-		String description = new Scanner(System.in).nextLine();
+		String description = sc.nextLine();
+		
+		// generate reportId
+		String reportId = "R" + (size + 1);
+		String userId = "U001";
 		
 		//get the current date
-		LocalDate now = LocalDate.now();
-		String reportDate = now.toString();
-		MaintenanceReport report = new MaintenanceReport(reportId, facilityId, userId, "NULL", description, reportDate, "NULL", "NULL", "PENDING", );
+		String reportDate = LocalDate.now().toString();
+		
+		MaintenanceReport report = new MaintenanceReport(reportId, facilityId, userId, "NULL", 
+									description, reportDate, "NULL", "NULL", "PENDING", "LOW");
 		reports.add(report);
 		size++;
 	}
@@ -52,19 +69,24 @@ public class MaintenanceManager {
 		} 
 	}
 	
-	public MaintenanceReport getMaintenanceHistory(){
+	public ArrayList<MaintenanceReport> getMaintenanceHistory(){
 		//WIP
 		//isCompleted
+		return reports;
 	}
+	
 	public void getFrequentIssues() {
 		//WIP
 	}
+	
 	public void generateMaintenanceReport() { //generate maintenance performance report
 		//WIP
 	}
+	
 	public void generateFrequentIssuesReport() { 
 		//WIP
 	}
+	
 	public void loadReports() { //WIP
 		ArrayList<MaintenanceReport> loaded = maintenanceStorage.load();
 		for(MaintenanceReport r : loaded) {
@@ -72,8 +94,9 @@ public class MaintenanceManager {
 			size++;
 		}
 	}
+	
 	public void saveReports() {
-		maintenanceStorage.save(reports,size);
+		maintenanceStorage.save(reports);
 	}
 
 
