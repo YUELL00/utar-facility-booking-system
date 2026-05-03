48aab244befabd6c12eeaa823827c0cd07cf964a
user <user@172.16.150.168>
2026-05-03 19:33:08 +0800

    test

--- src/maintenance/MaintenanceManager.java
+++ src/maintenance/MaintenanceManager.java
@@ -2,38 +2,47 @@ package maintenance;
 
 import java.time.*;
 import java.util.*;
+import storage.*;
+import user.*;
+import java.util.*;
 import java.time.LocalDate;
-
 import storage.MaintenanceStorage;
 
 public class MaintenanceManager {
-	private ArrayList<MaintenanceReport> reports;
-	private int size;
+	Scanner input = new Scanner(System.in);
+	
 	private MaintenanceStorage maintenanceStorage;
 	
-	public MaintenanceManager() {
-		reports = new ArrayList<>();
-		maintenanceStorage = new MaintenanceStorage("maintenance.txt");
-		size = 0;
+	private ArrayList<MaintenanceReport> reports;
+	
+	private int size;
+	
+	//constructor
+	public MaintenanceManager(){
+		this.reports = new ArrayList<>();
+		
+		this.maintenanceStorage = new MaintenanceStorage("D:\\MaintenanceStorage.txt");
+		
+		loadReports();
 	}
 	
-	public void createMaintenanceReport() { //WIP
+	public void createMaintenanceReport() { //feedback
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter the facility Id (Ex: F001): ");
 		String facilityId = sc.nextLine();
-		
+
 		System.out.println("Enter the issue description: ");
 		String description = sc.nextLine();
-		
+
 		// generate reportId
 		String reportId = "R" + (size + 1);
 		String userId = "U001";
-		
+
 		LocalDate reportDate = LocalDate.now();
 		
 		MaintenanceReport report = new MaintenanceReport(reportId, facilityId, userId, null, 
-									description, reportDate, null, null, "PENDING", "LOW");
+				description, reportDate, null, null, "PENDING", "LOW");
 		reports.add(report);
 		size++;
 	}
@@ -70,34 +79,99 @@ public class MaintenanceManager {
 	}
 	
 	public ArrayList<MaintenanceReport> getMaintenanceHistory(){
-		//WIP
-		//isCompleted
 		return reports;
 	}
 	
-	public void getFrequentIssues() {
-		//WIP
+	public void getFrequentIssues() {//WIP
+	    Map<String, Integer> wordCount = new HashMap<>();
+	    
+	    Set<String> stopWords = Set.of("the", "is", "and", "a", "to", "of", "in", "on");
+	    
+		for(MaintenanceReport r : reports) {
+			String desc = r.getDescription().toLowerCase();
+	        
+	        String[] words = desc.split("\\W+"); // 按非字母分割
+	        
+	        for (String word : words) {
+	            if (word.isEmpty() || stopWords.contains(word)) {
+	            	continue;
+	            }
+
+	            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+	        }
+		}
+		
+		System.out.println("Frequent Issues:");
+
+		for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
 	}
 	
 	public void generateMaintenanceReport() { //generate maintenance performance report
-		//WIP
-	}
-	
-	public void generateFrequentIssuesReport() { 
-		//WIP
-	}
-	
-	public void loadReports() { //WIP
-		ArrayList<MaintenanceReport> loaded = maintenanceStorage.load();
-		for(MaintenanceReport r : loaded) {
-			reports.add(r);
-			size++;
+
+		 //count
+		int assign = 0;
+		int inProgress = 0;
+		int complete = 0;
+		int reject = 0;
+		
+		int choice;
+		//Choose Time Range
+		System.out.println("===Time Range===");
+		System.out.println("1. Monthly");
+		System.out.println("2. Semester");
+		System.out.println("3. Yearly");
+		do {
+			System.out.println("Enter Your Choice: ");
+			choice = input.nextInt();
+			switch(choice) {
+			case 1:
+				
+			case 2:
+			
+			case 3:
+				
+			}
+		}while(choice <=1 && choice >=3);
+		
+		for(MaintenanceReport r : reports) {
+			switch(r.getStatus()) {
+			case "ASSIGNED":
+				assign++;
+				break;
+			case "IN_PROGRESS":
+				inProgress++;
+				break;
+			case "COMPLETED":
+				complete++;
+				break;
+			case "REJECTED":
+				reject++;
+				break;
+			}
 		}
+		System.out.println("=== Maintenance Preformance Report ===");
+		System.out.println("Assigned Task: " + assign);
+		System.out.println("Rejected Task: " + reject);
+		System.out.println("Task In Progress: " + inProgress);
+		System.out.println("Completed Task: " + complete);
+		
+		getFrequentIssues();
+		//WIP
+
+	}
+	
+	public void loadReports() {
+		
+		reports.clear();
+		
+		reports = maintenanceStorage.load();
+		
 	}
 	
 	public void saveReports() {
 		maintenanceStorage.save(reports);
 	}
-
 
 }
